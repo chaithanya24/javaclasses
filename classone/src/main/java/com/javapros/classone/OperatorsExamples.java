25e1195173412cff53be1f786a6d7ad9106cefda
chaithanya <chaithanyakumar.karuna@charter.com>
2018-12-28 21:44:04 -0700

    Classone: - Operator - Primitives.

---
+++ classone/src/main/java/com/javapros/classone/OperatorsExamples.java
@@ -0,0 +1,82 @@
+package com.javapros.classone;
+
+/**
+ * Explaining operators
+ * @author p2799013
+ *
+ */
+public class OperatorsExamples {
+
+	public static void main(String[] args) {
+		
+		/**
+		 * *,/,% > +,-
+		 */
+		
+		double f = 2+3*5/2+2;
+		System.out.println("x values:" +f);
+		
+		/**
+		 * relational operator.
+		 * <, <=, >, >=, ==, !=
+		 */
+		int x = 3;
+		if(x == 3) {
+			x = 6;
+			System.out.println("correct you passed the check");
+		}
+		
+		/**
+		 * Assignment operator.
+		 * ++, +=, -- etc
+		 */
+	    //Post increment ++, pre increment use.
+		//x = x+1;
+		x++;
+		
+		/*for(int i =0 ; i< 10 ;i++) {
+			System.out.println("i value::" +i);
+		}*/
+		System.out.println("x latest values:" +x);
+		System.out.println("x values: "+x);
+		
+		++x;
+		if(x == 8) {
+			System.out.println("inside increament");
+		}
+		
+		/**
+		 * logical operator.
+		 * TT -> T
+		 * TF-> T
+		 * FT->T
+		 * FF->F
+		 * TTT->T ...so on.
+		 */
+		
+		if(x == 8 || f == 12.0) {
+			System.out.println("condition satified");
+		}
+		
+		/**
+		 * 
+		 * Ternary operator.
+		 * 
+		 */
+		
+		
+		String str = (x == 8) ? "correct" : "wrong";
+		/**
+		 * in if block
+		 */
+		String str1 = "";
+		if(x == 8) {
+			str1 = "correct";
+		}else {
+			str1 = "wrong";
+		}
+		System.out.println(str1);
+
+	}
+
+}
---
+++ classone/src/main/java/com/javapros/classone/PrimitivesExample.java
@@ -0,0 +1,56 @@
+package com.javapros.classone;
+
+/**
+ * This is illustrate java primitives.
+ * @author p2799013
+ *
+ */
+public class PrimitivesExample {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * -128 to 127
+		 */
+		byte a = 127;
+		System.out.println("byte value: " + a);
+		
+		/*
+		 * range is -32768 to 32,767 
+		 */
+		short b = 3800;
+		System.out.println("short value:" + b);
+		
+		/*
+		 * range -2,147,483,648 to 2,147,483,647
+		 */
+		int c = 21474836;
+		System.out.println("int value:" + c);
+		
+		/*
+		 * range 2^63 to 2^63-1
+		 */
+		long d = 3457L;
+		System.out.println("long value:" + d);
+		
+		float e = 5/2f;
+		System.out.println("float value: " +e);
+		
+		double f = 4.345;
+		System.out.println("double value:" +f);
+		
+	
+		char g = 2; 
+		System.out.println("Charachter:" +g);
+		
+		boolean flag = true;
+		System.out.println("boolean: " + flag);
+		
+		/**
+		 * Assignment:
+		 * 1. get all types of primitives and play with each them.
+		 */
+
+	}
+
+}
